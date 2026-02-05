68188a1cd7f7862f622360273d20dbb3efe6e600
AxelmC06 <163697870+axelcortes6@users.noreply.github.com>
2026-02-04 18:26:10 -0600

    Add files via upload
    Codigo no queda igual que en la filmina de semana#2

---
+++ Tienda_AxelM 1/Tienda_AxelM/src/main/java/Tienda_AxelM/TiendaAxelMApplication.java
@@ -0,0 +1,16 @@
+package Tienda_AxelM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAxelMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAxelMApplication.class, args);
+                
+
+
+
+}
+}
